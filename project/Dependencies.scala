75af150f09c63b626c4313cd5aabe17a7c047eae
Scala Steward <me@scala-steward.org>
2021-06-21 20:53:51 +0200

    Update http4s-backend to 3.3.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
 
     val catsEffect = "3.1.1"
     val http4s = "0.23.0-RC1"
-    val sttpClient3 = "3.3.6"
+    val sttpClient3 = "3.3.7"
 
     val kindProjector = "0.13.0"
     val betterMonadicFor = "0.3.1"
